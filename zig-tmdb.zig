318358de38f1dea00f508d95eb58b7b18dafa58a
Clancy Woodburn <woodburn.clancy@gmail.com>
2024-12-31 14:14:44 +1100

    Make errors return instead of treating as unreachable

--- zig-tmdb.zig
+++ zig-tmdb.zig
@@ -49,9 +49,10 @@ pub const TmdbSession = struct {
         return body_buf[0..body_len];
     }
 
-    pub fn get(self: TmdbSession, query: Query) !TmdbResponse {
-        const response = try self.launchRequest(query.bake(self.alloc));
+    pub fn get(self: TmdbSession, allocator: std.mem.Allocator, query: Query) !TmdbResponse {
+        const response = try self.launchRequest(try query.bake(allocator));
 
+        std.debug.print("{s}\n", .{response});
         // TODO: surely there is a better way of doing this?
         switch (query.response_type) {
             .search_movie => {
@@ -90,26 +91,36 @@ pub const Field = struct {
         return Field{ .value = QueryValue{ .boolean = params.val }, .label = params.label, .field_type = params.field_type };
     }
 
-    pub fn fromString(params: struct { label: []const u8 = "", val: []const u8, field_type: FieldType = .query_param }) Field {
-        return Field{ .value = QueryValue{ .string = params.val }, .label = params.label, .field_type = params.field_type };
+    pub fn fromString(allocator: std.mem.Allocator, params: struct { label: []const u8 = "", val: []const u8, field_type: FieldType = .query_param }) !Field {
+        return Field{ .value = QueryValue{ .string = try std.mem.Allocator.dupe(allocator, u8, params.val) }, .label = params.label, .field_type = params.field_type };
     }
 
     pub fn fromInt(params: struct { label: []const u8 = "", val: u32, field_type: FieldType = .query_param }) Field {
         return Field{ .value = QueryValue{ .int = params.val }, .label = params.label, .field_type = params.field_type };
     }
 
-    pub fn formatLabel(self: Field, allocator: std.mem.Allocator) []const u8 {
+    pub fn formatLabel(self: Field, allocator: std.mem.Allocator) ![]const u8 {
         switch (self.value) {
             .string => |string| {
                 if (self.field_type != .query_param) return string;
                 var encoded_string = std.ArrayList(u8).init(allocator);
                 defer encoded_string.deinit();
-                std.Uri.Component.percentEncode(encoded_string.writer(), string, isValidUnencoded) catch unreachable;
-                return std.mem.Allocator.dupe(allocator, u8, encoded_string.items) catch unreachable;
-            }, // TODO: if this is a query parameter, should have certain characters swapped (see: https://en.wikipedia.org/wiki/Percent-encoding)
+                // std.debug.print("{s}\n\n", .{string});
+                try std.Uri.Component.percentEncode(encoded_string.writer(), string, isValidUnencoded);
+                return try std.mem.Allocator.dupe(allocator, u8, encoded_string.items);
+            },
             .boolean => |boolean| return if (boolean) "true" else "false",
-            .int => |int| return std.fmt.allocPrint(allocator, "{d}", .{int}) catch unreachable,
+            .int => |int| return try std.fmt.allocPrint(allocator, "{d}", .{int}),
             else => return "",
+        }
+    }
+
+    pub fn deinit(self: Field, allocator: std.mem.Allocator) void {
+        switch (self.value) {
+            .string => |string| {
+                allocator.free(string);
+            },
+            else => {},
         }
     }
 
@@ -129,7 +140,7 @@ pub const Query = struct {
     endpoint: []const u8,
     response_type: ResponseType,
 
-    pub fn bake(self: Query, allocator: std.mem.Allocator) []const u8 {
+    pub fn bake(self: Query, allocator: std.mem.Allocator) ![]const u8 {
         var base_string = std.ArrayList(u8).init(allocator);
         var base_writer = base_string.writer();
 
@@ -141,12 +152,12 @@ pub const Query = struct {
         for (self.fields) |field| {
             switch (field.field_type) {
                 .query_param => {
-                    const val = field.formatLabel(allocator);
+                    const val = try field.formatLabel(allocator);
                     defer if (field.requiresDeinit()) allocator.free(val);
-                    writer.print("{s}={s}&", .{ field.label, val }) catch unreachable;
+                    try writer.print("{s}={s}&", .{ field.label, val });
                 },
                 .path_param => {
-                    path_params.append(field) catch unreachable;
+                    try path_params.append(field);
                 },
                 else => continue,
             }
@@ -156,21 +167,21 @@ pub const Query = struct {
         var i: usize = 0;
         while (i < self.endpoint.len) {
             if (self.endpoint[i] == '{') {
-                const end = std.mem.indexOf(u8, self.endpoint[i + 1 ..], "}") orelse unreachable;
+                const end = std.mem.indexOf(u8, self.endpoint[i + 1 ..], "}") orelse return error.BracketMismatch;
                 const key = self.endpoint[i + 1 .. i + 1 + end];
 
                 for (path_params.items) |param| {
                     if (std.mem.eql(u8, param.label, key)) {
-                        const val = param.formatLabel(allocator);
+                        const val = try param.formatLabel(allocator);
                         defer if (param.requiresDeinit()) allocator.free(val);
-                        _ = base_writer.write(val) catch unreachable;
+                        _ = try base_writer.write(val);
                         break;
                     }
                 }
 
                 i += 1 + end + 1;
             } else {
-                _ = base_writer.writeByte(self.endpoint[i]) catch unreachable;
+                _ = try base_writer.writeByte(self.endpoint[i]);
                 i += 1;
             }
         }
@@ -178,52 +189,58 @@ pub const Query = struct {
         // const query_string = toQueryString(allocator, query_params, query_labels) catch unreachable;
         // defer allocator.free(query_string);
         if (query_string.items.len > 0) {
-            return std.mem.join(allocator, "?", &.{ base_string.items, query_string.items }) catch unreachable;
+            return try std.mem.join(allocator, "?", &.{ base_string.items, query_string.items });
         }
 
-        return std.fmt.allocPrint(allocator, "{s}", .{base_string.items}) catch unreachable;
+        return try std.fmt.allocPrint(allocator, "{s}", .{base_string.items});
         //const out = try std.fmt.allocPrint(allocator, "{s}", .{self.query});
         //defer allocator.free(out);
         //return try std.mem.join(allocator, "?", &.{ TEMPLATE, out });
     }
 
-    pub fn searchMovie(allocator: std.mem.Allocator, params: struct { query: []const u8, include_adult: bool = false, language: []const u8 = "en-US", primary_release_year: ?u32 = null, page: u32 = 1, region: ?[]u8 = undefined, year: ?u32 = null }) Query {
-        var fields = std.ArrayList(Field).init(allocator);
-        defer fields.deinit();
-        fields.append(Field.fromString(.{ .label = "query", .val = params.query })) catch unreachable;
-        fields.append(Field.fromBoolean(.{ .label = "include_adult", .val = params.include_adult })) catch unreachable;
-        fields.append(Field.fromString(.{ .label = "language", .val = params.language })) catch unreachable;
-        fields.append(Field.fromInt(.{ .label = "page", .val = params.page })) catch unreachable;
-        if (params.year != null) fields.append(Field.fromInt(.{ .label = "year", .val = params.year orelse unreachable })) catch unreachable;
-        if (params.primary_release_year != null) fields.append(Field.fromInt(.{ .label = "primary_release_year", .val = params.primary_release_year orelse unreachable })) catch unreachable;
-        if (params.region != null) fields.append(Field.fromString(.{ .label = "region", .val = params.region orelse unreachable })) catch unreachable;
-
-        return Query{ .fields = std.mem.Allocator.dupe(allocator, Field, fields.items[0..]) catch unreachable, .endpoint = "https://api.themoviedb.org/3/search/movie", .response_type = ResponseType.search_movie };
+    pub fn deinit(self: Query, allocator: std.mem.Allocator) void {
+        for (self.fields) |field| {
+            field.deinit(allocator);
+        }
     }
 
-    pub fn movieDetails(allocator: std.mem.Allocator, params: struct { movie_id: u32, append_to_response: ?[]const u8 = null }) Query {
+    pub fn searchMovie(allocator: std.mem.Allocator, params: struct { query: []const u8, include_adult: bool = false, language: []const u8 = "en-US", primary_release_year: ?u32 = null, page: u32 = 1, region: ?[]u8 = undefined, year: ?u32 = null }) !Query {
         var fields = std.ArrayList(Field).init(allocator);
         defer fields.deinit();
-        fields.append(Field.fromInt(.{ .label = "movie_id", .val = params.movie_id, .field_type = FieldType.path_param })) catch unreachable;
-        if (params.append_to_response != null) fields.append(Field.fromString(.{ .label = "append_to_response", .val = params.append_to_response orelse unreachable })) catch unreachable;
+        try fields.append(try Field.fromString(allocator, .{ .label = "query", .val = params.query }));
+        try fields.append(Field.fromBoolean(.{ .label = "include_adult", .val = params.include_adult }));
+        try fields.append(try Field.fromString(allocator, .{ .label = "language", .val = params.language }));
+        try fields.append(Field.fromInt(.{ .label = "page", .val = params.page }));
+        if (params.year) |y| try fields.append(Field.fromInt(.{ .label = "year", .val = y }));
+        if (params.primary_release_year) |pry| try fields.append(Field.fromInt(.{ .label = "primary_release_year", .val = pry }));
+        if (params.region) |r| try fields.append(try Field.fromString(allocator, .{ .label = "region", .val = r }));
 
-        return Query{ .fields = std.mem.Allocator.dupe(allocator, Field, fields.items[0..]) catch unreachable, .endpoint = "https://api.themoviedb.org/3/movie/{movie_id}", .response_type = ResponseType.movie_details };
+        return Query{ .fields = try std.mem.Allocator.dupe(allocator, Field, fields.items[0..]), .endpoint = "https://api.themoviedb.org/3/search/movie", .response_type = ResponseType.search_movie };
     }
 
-    pub fn movieCredits(allocator: std.mem.Allocator, params: struct { movie_id: u32 }) Query {
+    pub fn movieDetails(allocator: std.mem.Allocator, params: struct { movie_id: u32, append_to_response: ?[]const u8 = null }) !Query {
         var fields = std.ArrayList(Field).init(allocator);
         defer fields.deinit();
-        fields.append(Field.fromInt(.{ .label = "movie_id", .val = params.movie_id, .field_type = FieldType.path_param })) catch unreachable;
+        try fields.append(Field.fromInt(.{ .label = "movie_id", .val = params.movie_id, .field_type = FieldType.path_param }));
+        if (params.append_to_response) |atr| try fields.append(try Field.fromString(allocator, .{ .label = "append_to_response", .val = atr }));
 
-        return Query{ .fields = std.mem.Allocator.dupe(allocator, Field, fields.items[0..]) catch unreachable, .endpoint = "https://api.themoviedb.org/3/movie/{movie_id}/credits", .response_type = ResponseType.movie_credits };
+        return Query{ .fields = try std.mem.Allocator.dupe(allocator, Field, fields.items[0..]), .endpoint = "https://api.themoviedb.org/3/movie/{movie_id}", .response_type = ResponseType.movie_details };
     }
 
-    pub fn movieExternalIds(allocator: std.mem.Allocator, params: struct { movie_id: u32 }) Query {
+    pub fn movieCredits(allocator: std.mem.Allocator, params: struct { movie_id: u32 }) !Query {
         var fields = std.ArrayList(Field).init(allocator);
         defer fields.deinit();
-        fields.append(Field.fromInt(.{ .label = "movie_id", .val = params.movie_id, .field_type = FieldType.path_param })) catch unreachable;
+        try fields.append(Field.fromInt(.{ .label = "movie_id", .val = params.movie_id, .field_type = FieldType.path_param }));
 
-        return Query{ .fields = std.mem.Allocator.dupe(allocator, Field, fields.items[0..]) catch unreachable, .endpoint = "https://api.themoviedb.org/3/movie/{movie_id}/external_ids", .response_type = ResponseType.movie_external_ids };
+        return Query{ .fields = try std.mem.Allocator.dupe(allocator, Field, fields.items[0..]), .endpoint = "https://api.themoviedb.org/3/movie/{movie_id}/credits", .response_type = ResponseType.movie_credits };
+    }
+
+    pub fn movieExternalIds(allocator: std.mem.Allocator, params: struct { movie_id: u32 }) !Query {
+        var fields = std.ArrayList(Field).init(allocator);
+        defer fields.deinit();
+        try fields.append(Field.fromInt(.{ .label = "movie_id", .val = params.movie_id, .field_type = FieldType.path_param }));
+
+        return Query{ .fields = try std.mem.Allocator.dupe(allocator, Field, fields.items[0..]), .endpoint = "https://api.themoviedb.org/3/movie/{movie_id}/external_ids", .response_type = ResponseType.movie_external_ids };
     }
 };
 
@@ -237,11 +254,12 @@ const Genre = struct { id: u32, name: []u8 };
 const ProductionCompany = struct { id: u32, logo_path: ?[]u8, name: []u8, origin_country: []u8 };
 const ProductionCountry = struct { iso_3166_1: []u8, name: []u8 };
 const SpokenLanguage = struct { english_name: []u8, iso_639_1: []u8, name: []u8 };
+const Collection = struct { id: u32, name: []u8, poster_path: ?[]u8, backdrop_path: []u8 };
 
 const SearchMovieResponseObject = struct { adult: bool, backdrop_path: ?[]u8, genre_ids: []u32, id: u32, original_language: []u8, original_title: []u8, overview: []u8, popularity: f32, poster_path: ?[]u8, release_date: ?[]u8, title: []u8, video: bool, vote_average: f32, vote_count: u32 };
 const SearchMovieResponse = struct { page: u32, results: []SearchMovieResponseObject, total_pages: u32, total_results: u32 };
 
-const MovieDetailsResponse = struct { adult: bool, backdrop_path: []u8, belongs_to_collection: ?[]u8, budget: u32, genres: []Genre, homepage: []u8, id: u32, imdb_id: ?[]u8, origin_country: [][]u8, original_language: ?[]u8, original_title: []u8, overview: []u8, popularity: f32, poster_path: ?[]u8, production_companies: []ProductionCompany, production_countries: []ProductionCountry, release_date: ?[]u8, revenue: u32, runtime: u32, spoken_languages: []SpokenLanguage, status: ?[]u8, tagline: ?[]u8, title: []u8, video: bool, vote_average: f32, vote_count: u32, credits: ?MovieCreditsResponse = null, external_ids: ?MovieExternalIdsResponse = null };
+const MovieDetailsResponse = struct { adult: bool, backdrop_path: ?[]u8, belongs_to_collection: ?Collection, budget: u32, genres: []Genre, homepage: ?[]u8, id: u32, imdb_id: ?[]u8, origin_country: [][]u8, original_language: ?[]u8, original_title: []u8, overview: []u8, popularity: f32, poster_path: ?[]u8, production_companies: []ProductionCompany, production_countries: []ProductionCountry, release_date: ?[]u8, revenue: u32, runtime: u32, spoken_languages: []SpokenLanguage, status: ?[]u8, tagline: ?[]u8, title: []u8, video: bool, vote_average: f32, vote_count: u32, credits: ?MovieCreditsResponse = null, external_ids: ?MovieExternalIdsResponse = null };
 
 const MovieCreditsResponse = struct { id: u32 = 0, cast: []struct { adult: bool = true, gender: u32, id: u32, known_for_department: []u8, name: []u8, original_name: []u8, popularity: f32 = 0, profile_path: ?[]u8, cast_id: u32 = 0, character: []u8, credit_id: []u8, order: u32 = 0 }, crew: []struct { adult: bool = true, gender: u32, id: u32, known_for_department: []u8, name: []u8, original_name: []u8, popularity: f32 = 0, profile_path: ?[]u8, credit_id: []u8, department: []u8, job: []u8 } };
 const MovieExternalIdsResponse = struct { id: u32 = 0, imdb_id: ?[]u8, wikidata_id: ?[]u8, facebook_id: ?[]u8, instagram_id: ?[]u8, twitter_id: ?[]u8 };
